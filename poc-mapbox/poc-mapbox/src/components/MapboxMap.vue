f64e0f1fad2120915f28ae0cda43a3c8558f9059
Maurits Maas <367868@student.fontys.nl>
2019-04-24 13:20:41 +0200

    car move to point

--- poc-mapbox/poc-mapbox/src/components/MapboxMap.vue
+++ poc-mapbox/poc-mapbox/src/components/MapboxMap.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="fullscreen" id='simulation'></div>
+
+    <div class="fullscreen" id='simulation'>
+        <button v-on:click="moveTo" id='replay'>Move to</button>
+        <input v-model="longitude" placeholder="lng">
+        <input v-model="latitude" placeholder="lat">
+    </div>
+
 </template>
 
 <script>
@@ -11,10 +17,12 @@
             return {
                 map: null,
                 origin: [7.460684, 51.513106],
-                destination: [13.421612, 52.560955],
+                destination: null,
                 arc: [],
-                steps: 500,
-                counter: 0
+                steps: 1,
+                counter: 0,
+                longitude: null,
+                latitude: null
             }
         },
         computed: {
@@ -63,36 +71,14 @@
                 zoom: 5 // starting zoom
             });
 
-            self.nextMovement(self.destination)
-
-            for (var i = 0; i < self.lineDistance; i += self.lineDistance / self.steps) {
-                var segment = turf.along(self.route.features[0], i, 'kilometers');
-                self.arc.push(segment.geometry.coordinates);
-
-            }
-
-            self.route.features[0].geometry.coordinates = self.arc;
-            self.map.on('load', function () {
-// Add a source and layer displaying a point which will be animated in a circle.
-                self.map.addSource('route', {
-                    "type": "geojson",
-                    "data": self.route
-                });
+            self.map.on('load', function ()
+            {
 
                 self.map.addSource('point', {
                     "type": "geojson",
                     "data": self.point
                 });
 
-                self.map.addLayer({
-                    "id": "route",
-                    "source": "route",
-                    "type": "line",
-                    "paint": {
-                        "line-width": 2,
-                        "line-color": "#007cbf"
-                    }
-                });
 
                 self.map.addLayer({
                     "id": "point",
@@ -106,40 +92,28 @@
                     }
                 });
 
-                function animate() {
-                    // Update point geometry to a new position based on counter denoting
-                    // the index to access the arc.
-                    self.point.features[0].geometry.coordinates = self.route.features[0].geometry.coordinates[self.counter];
-
-                    // Calculate the bearing to ensure the icon is rotated to match the route arc
-                    // The bearing is calculate between the current point and the next point, except
-                    // at the end of the arc use the previous point and the current point
-                    self.point.features[0].properties.bearing = turf.bearing(
-                        turf.point(self.route.features[0].geometry.coordinates[self.counter >= self.steps ? self.counter - 1 : self.counter]),
-                        turf.point(self.route.features[0].geometry.coordinates[self.counter >= self.steps ? self.counter : self.counter + 1])
-                    );
-
-                    // Update the source with self new data.
-                    self.map.getSource('point').setData(self.point);
-
-                    // Request the next frame of animation so long the end has not been reached.
-                    if (self.counter < self.steps) {
-                        requestAnimationFrame(animate);
-                    }
-
-                    self.counter = self.counter + 1;
-                }
-
-                // Start the animation.
-                animate(self.counter);
             })
         },
         methods : {
             nextMovement(payload) {
-                var nextMovementX = payload[0] + 0.001;
-                var nextMovementY = payload[1] + 0.001;
+                var self = this
+                var nextMovementX = payload[0];
+                var nextMovementY = payload[1];
                 this.origin = [payload[0], payload[1]];
                 this.destination = [nextMovementX, nextMovementY];
+
+                self.animate()
+            },
+            animate () {
+                var self = this
+
+                // Update the source with self new data.
+                self.map.getSource('point').setData(self.point);
+                requestAnimationFrame(self.animate);
+            },
+            moveTo: function () {
+                var coord = [ this.longitude, this.latitude ]
+                this.nextMovement( coord )
             }
         }
     }
@@ -165,10 +139,11 @@
         color: #42b983;
     }
 
-    #map {
+    #simulation {
         position: absolute;
         top: 0;
         bottom: 0;
         width: 100%;
+        height: 100%;
     }
 </style>
